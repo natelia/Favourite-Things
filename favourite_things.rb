ba45af4e5d69541e1113c7307cb06d559152830c
natelia <natalia.jadwiga.gajewska@gmail.com>
2023-11-01 20:57:13 +0100

    first commit

---
+++ favourite_things.rb
@@ -0,0 +1,30 @@
+def favourite_food(name)
+
+  if name == "Lister"
+      return "vindaloo"
+  end
+  
+  if name == "Rimmer"
+      return "mashed potatoes"
+  end
+  "hard to say.. maybe fried plantain?"
+
+end
+
+def favourite_drink(name)
+  if name == "Jean-Luc"
+      "tea, Earl Grey, hot"
+  elsif name == "Kathryn"
+      "coffee, black"
+  else
+      "perhaps... horchata?"
+  end
+end
+
+puts favourite_food("Rimmer")
+puts favourite_food("Lister")
+puts favourite_food("Cassandra")
+
+puts favourite_drink("Kathryn")
+puts favourite_drink("Q")
+puts favourite_drink("Jean-Luc")
